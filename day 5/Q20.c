6e41e52efa419693e7c412169ac6a0b159057bc6
shryscodes <25112cn401@glbitm.ac.in>
2026-06-05 09:34:48 +0530

    Create Q20.c

---
+++ day 5/Q20.c
@@ -0,0 +1,43 @@
+// Write a program to Find largest prime factor.
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter a number: \n");
+    scanf("%d", &n);
+    int primes[n];
+    int count=0;
+    for (int i = n; i>1; i--)
+    {
+        int isprime=1;
+        for (int j = 2; j <=i; j++)
+        {
+            
+            if (i%j==0)
+            {
+                isprime=0;
+                break;
+            }
+            
+        }
+        if (isprime)
+        {
+            primes[count]=i;
+            count++;
+        }
+        
+        
+        
+    }
+    for (int i = count-1; i>=0 ; i--)
+    {
+        if (n%primes[i]==0)
+        {
+            printf("%d is the largest prime factor of %d", primes[i], n);
+            break;
+        }
+        
+    }
+    
+    
+    return 0;
+}
