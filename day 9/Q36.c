6ca464b93d683ee42e43b461f3d7a930fd01f4cd
shryscodes <25112cn401@glbitm.ac.in>
2026-06-06 13:56:50 +0530

    Day 9 uploaded

---
+++ day 9/Q36.c
@@ -0,0 +1,29 @@
+  // Write a program to Print hollow square
+// pattern.
+// *****
+// * *
+// * *
+// * *
+// *****
+#include<stdio.h>
+int main()
+{
+    int n; 
+    printf("Enter the size of square: \n");
+    scanf("%d", &n);
+    for (int i = 1; i <=n; i++)
+    {
+        for (int j = 1; j <=n; j++)
+        {
+            if(i==1||j==1||i==n||j==n)
+            {
+                printf("* ");
+            }
+            else
+            printf("  ");
+        }
+        printf("\n");
+        
+    }
+    
+}
