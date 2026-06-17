91082cfa99c357efd18516193ebec5f5c1701297
sHREY4S <25112cn401@glbitm.ac.in>
2026-06-17 11:47:46 +0530

    Create Q63.java

---
+++ day 16/Q63.java
@@ -0,0 +1,40 @@
+// Write a program to Find pair with given sum.
+
+import java.util.Scanner;
+
+public class Q63 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the size of array: ");
+        int n = sc.nextInt();
+
+        if (n <= 0) {
+            System.out.println("Size must be greater than 0!");
+            sc.close();
+            return;
+        }
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter " + n + " elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter the sum: ");
+        int sum = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] + arr[j] == sum) {
+                    System.out.println(arr[i] + " + " + arr[j] + " = " + sum);
+                }
+            }
+        }
+
+        sc.close();
+    }
+}
