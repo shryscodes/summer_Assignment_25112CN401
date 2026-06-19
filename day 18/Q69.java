3deadef755e6524a5939983a5b6bb0f71c5af0d3
sHREY4S <25112cn401@glbitm.ac.in>
2026-06-19 14:47:22 +0530

    Create Q69.java

---
+++ day 18/Q69.java
@@ -0,0 +1,41 @@
+// Write a program to Bubble sort.
+import java.util.Scanner;
+
+public class Q69 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the size of array: ");
+        int n = sc.nextInt();
+
+        if (n <= 0) {
+            System.out.println("Size must be greater than 0!");
+            sc.close();
+            return;
+        }
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter " + n + " elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+        System.out.print("Sorted Array: ");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+
+        sc.close();
+    }
+}
